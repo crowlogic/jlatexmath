aa96cbcce1fde924cc6e3b364be13df9e800c7e3
Stephen A. Crowley <crow@outsider.trading>
2023-12-05 20:08:44 -0600

    dusting off

--- jlatexmath/src/main/java/module-info.java
+++ jlatexmath/src/main/java/module-info.java
@@ -2,7 +2,8 @@ module jlatexmath
 {
   requires transitive java.base;
   requires transitive java.desktop;
-
+  requires transitive java.xml;
+  
   exports org.scilab.forge.jlatexmath;
   exports org.scilab.forge.jlatexmath.cache;
   exports org.scilab.forge.jlatexmath.cyrillic;
