7d3d6041573611830a453612800d9df144d6b479
Stephen A. Crowley <crow@outsider.trading>
2024-01-27 01:28:35 -0600

    defop

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,10 +3,6 @@ module jlatexmath
   requires java.base;
   requires transitive java.desktop;
   requires transitive java.xml;
-  requires transitive fop;
-  requires transitive xmlgraphics.commons;
-  requires transitive fop.core;
-
   exports org.scilab.forge.jlatexmath;
   exports org.scilab.forge.jlatexmath.cache;
   exports org.scilab.forge.jlatexmath.cyrillic;
