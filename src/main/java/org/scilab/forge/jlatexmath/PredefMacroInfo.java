707ab92aaee32e68939d575b2dad38d8f177b407
Stephen Crowley <stephencrowley214@gmail.com>
2024-03-09 23:39:12 -0600

    fork the forking forkers: WARNING: Unknown module: arb4j specified to --enable-native-access Exception in thread "main" org.scilab.forge.jlatexmath.ParseException: Problem with command array@@env at position 0:856 Problem with command mbox at position 0:83 fonts/language_cyrillic.xml 	at jlatexmath/org.scilab.forge.jlatexmath.PredefMacroInfo.invokeID(PredefMacroInfo.java:601) 	at jlatexmath/org.scilab.forge.jlatexmath.PredefMacroInfo.invoke(PredefMacroInfo.java:73) 	at jlatexmath/org.scilab.forge.jlatexmath.TeXParser.processCommands(TeXParser.java:1743) 	at jlatexmath/org.scilab.forge.jlatexmath.TeXParser.processEscape(TeXParser.java:1535) 	at jlatexmath/org.scilab.forge.jlatexmath.TeXParser.parse(TeXParser.java:845) 	at jlatexmath/org.scilab.forge.jlatexmath.TeXParser.getArgument(TeXParser.java:1241) 	at jlatexmath/org.scilab.forge.jlatexmath.TeXParser.parse(TeXParser.java:857) 	at jlatexmath/org.scilab.forge.jlatexmath.TeXFormula.<init>(TeXFormula.java:296) 	at jlatexmath/org.scilab.forge.jlatexmath.TeXFormula.<init>(TeXFormula.java:275) 	at jlatexmath/org.scilab.forge.jlatexmath.examples.basic.Example1.main(Example1.java:87) Caused by: org.scilab.forge.jlatexmath.ParseException: Problem with command mbox at position 0:83 fonts/language_cyrillic.xml 	at jlatexmath/org.scilab.forge.jlatexmath.PredefMacroInfo.invokeID(PredefMacroInfo.java:601) 	at jlatexmath/org.scilab.forge.jlatexmath.PredefMacroInfo.invoke(PredefMacroInfo.java:73) 	at jlatexmath/org.scilab.forge.jlatexmath.TeXParser.processCommands(TeXParser.java:1743) 	at jlatexmath/org.scilab.forge.jlatexmath.TeXParser.processEscape(TeXParser.java:1535) 	at jlatexmath/org.scilab.forge.jlatexmath.TeXParser.parse(TeXParser.java:845) 	at jlatexmath/org.scilab.forge.jlatexmath.PredefMacros.arrayATATenv_macro(PredefMacros.java:1355) 	at jlatexmath/org.scilab.forge.jlatexmath.PredefMacroInfo.invokeID(PredefMacroInfo.java:288) 	... 9 more Caused by: org.scilab.forge.jlatexmath.XMLResourceParseException: fonts/language_cyrillic.xml 	at jlatexmath/org.scilab.forge.jlatexmath.DefaultTeXFontParser.<init>(DefaultTeXFontParser.java:223) 	at jlatexmath/org.scilab.forge.jlatexmath.DefaultTeXFont.addTeXFontDescription(DefaultTeXFont.java:182) 	at jlatexmath/org.scilab.forge.jlatexmath.DefaultTeXFont.addAlphabet(DefaultTeXFont.java:219) 	at jlatexmath/org.scilab.forge.jlatexmath.DefaultTeXFont.addAlphabet(DefaultTeXFont.java:255) 	at jlatexmath/org.scilab.forge.jlatexmath.TeXParser.convertCharacter(TeXParser.java:1387) 	at jlatexmath/org.scilab.forge.jlatexmath.TeXParser.parse(TeXParser.java:938) 	at jlatexmath/org.scilab.forge.jlatexmath.TeXFormula.<init>(TeXFormula.java:438) 	at jlatexmath/org.scilab.forge.jlatexmath.PredefMacros.mbox_macro(PredefMacros.java:707) 	at jlatexmath/org.scilab.forge.jlatexmath.PredefMacroInfo.invokeID(PredefMacroInfo.java:122) 	... 15 more Caused by: java.lang.IllegalArgumentException: InputStream cannot be null 	at java.xml/javax.xml.parsers.DocumentBuilder.parse(DocumentBuilder.java:118) 	at jlatexmath/org.scilab.forge.jlatexmath.DefaultTeXFontParser.<init>(DefaultTeXFontParser.java:219) 	... 23 more

--- src/main/java/org/scilab/forge/jlatexmath/PredefMacroInfo.java
+++ src/main/java/org/scilab/forge/jlatexmath/PredefMacroInfo.java
@@ -599,7 +599,7 @@ class PredefMacroInfo extends
     catch (Exception e)
     {
       throw new ParseException("Problem with command " + args[0] + " at position " + tp.getLine() + ":" + tp.getCol()
-                    + "\n" + e.getMessage());
+                    + "\n" + e.getMessage(),e);
     }
   }
 }
